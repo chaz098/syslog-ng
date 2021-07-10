a8af20868172872a1f454313cc3d5dd3ce57188a
chaz098 <chazroman2@gmail.com>
2021-07-09 23:24:55 -0400

    adding updated bash script

--- syslog-ng_server.sh
+++ syslog-ng_server.sh
@@ -3,21 +3,21 @@
 #installs syslog-ng
 syslog_install () {
 	sudo apt install syslog-ng -y
-done
+
 }
 
 #change main config file to .bak and create new config file.
 config () {
 	mv /etc/syslog-ng/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf.bak
 	mv $PWD/syslog-ng.conf /etc/syslog-ng/
-done
+
 }
 
 #restarts service with
 service_restart () {
 	service syslog-ng restart
 	service syslog-ng status
-done
+
 }
 
 #checks to make sure that the script is being ran as root
