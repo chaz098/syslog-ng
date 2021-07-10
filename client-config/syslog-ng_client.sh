a65a2e952f210031cb4aebb6084a34d8bf0aecee
chaz098 <chazroman2@gmail.com>
2021-07-10 09:42:34 -0400

    updating script and adding blank config file

--- client-config/syslog-ng_client.sh
+++ client-config/syslog-ng_client.sh
@@ -1,1 +1,34 @@
 #!/bin/bash/
+
+#installs syslog-ng
+syslog_install () {
+	sudo apt install syslog-ng -y
+
+}
+
+#change main config file to .bak and create new config file.
+config () {
+	mv /etc/syslog-ng/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf.bak
+	mv $PWD/syslog-ng.conf /etc/syslog-ng/
+
+}
+
+#restarts service with
+service_restart () {
+	service syslog-ng restart
+	service syslog-ng status
+
+}
+
+#checks to make sure that the script is being ran as root
+sudo_check () {
+	if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+	    echo "Not running as root"
+	    exit
+	fi	
+}
+
+sudo_check
+syslog_install
+config
+service_restart
